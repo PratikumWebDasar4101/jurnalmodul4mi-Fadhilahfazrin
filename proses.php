7a3af37287a1e62d93261888579fc8de08a7b844
Fadhilah Fazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-09-24 10:56:19 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,26 @@
 <?php
+$foto = $_FILES['dokumen']['name'];
+if (isset($_POST['submit'])) {
+	move_uploaded_file($_FILES['dokumen']['tmp_name'], "upload/".$_FILES['dokumen']['name']);
+	echo "<h3>Foto anda :</h3>";
+	echo "<br>";
+	echo "<img src = 'upload/$foto'>";
+}
 
+$hobi = $_POST["hobi"];
+echo "<br>";
+echo "<h3>Hobi yang anda pilih :</h3>";
+echo "<br>";
+foreach ($hobi as $nilai) {
+	echo "- $nilai";
+	echo "<br>";
+}
+
+if (isset($_POST['delete'])) {
+	unset($hobi);
+}
 ?>
+
+<form action="halamanakhir.php">
+	<input type="submit" name="delete" value="Hapus">
+</form>
